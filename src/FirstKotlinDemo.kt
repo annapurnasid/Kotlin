2dd24840a2940eb46ee49febe20693880f122e35
home <smartherd@gmail.com>
2017-09-24 21:56:34 +0530

    Loops and Iterators - FOR Loop

--- src/FirstKotlinDemo.kt
+++ src/FirstKotlinDemo.kt
@@ -1,28 +1,14 @@
 /*
-* IF as expression
+* Loops and Iterators
 *
-* WHEN as expression
+* FOR Loop
 * */
 fun main(args : Array<String>) {
 
-    var num1 = 4
-    var num2 = 5
-    var big = if(num1 > num2) {
-                   println("check1")
-                   num1
-               } else {
-                   println("check2")
-                   num2
-               }
-
-    println("Bigger of $num1 and $num2 -> $big")
-
-    var side = 1
-    var output = when(side) {
-        1 -> "Straight Line"
-        3, 6, 7 -> "Triangle/Hexagon/Septagon"
-        4 -> "Square"
-        else -> "$side not recorded"
+    // i -> counter variable
+    // Condition verification
+    // inc/dec the counter
+    for(i in 1..5){
+        println("Hello World")
     }
-    println(output)
 }
