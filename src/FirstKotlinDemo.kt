001525e269f1998cb6fc4cf7486807e9d4ae46ee
home <smartherd@gmail.com>
2017-09-24 11:43:06 +0530

    Comments, Data Types and Variables

--- src/FirstKotlinDemo.kt
+++ src/FirstKotlinDemo.kt
@@ -1,5 +1,20 @@
+/*
+* Comments
+* Variables, Constants and Data types
+* */
 fun main(args : Array<String>) {
-    println("Hello World")
-    print(5+5)
-    print("Envato Tuts+")
+    var element :String // Mutable
+    element = "circle"
+    println(element)
+    element = "square"
+    println(element)
+    val figure : String = "rectangle" // Immutable
+
+    // data types examples
+    var side : Int = 3
+    var length : Float = 2.5f
+    var isGeometric : Boolean = true
+    println("area of " + element + " is " + side*side)
+
 }
+
