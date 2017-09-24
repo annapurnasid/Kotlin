0d3a96f5e7548900aca5cdeeffe5c43a76c7a6e7
home <smartherd@gmail.com>
2017-09-24 22:18:46 +0530

    Loops and Iterators - While and Do while Loop

--- src/FirstKotlinDemo.kt
+++ src/FirstKotlinDemo.kt
@@ -8,7 +8,18 @@ fun main(args : Array<String>) {
     // i -> counter variable
     // Condition verification
     // inc/dec the counter
-    for(i in 1..5){
-        println("Hello World")
+    var i :Int = 1
+    while(i < 10){
+        if (i % 2 == 0)
+            println(i)
+        i++
     }
+
+    println("Odd Numbers - Do While loop")
+    i = 1
+    do {
+        if (i % 2 != 0)
+            println(i)
+        i++
+    } while(i < 10)
 }
