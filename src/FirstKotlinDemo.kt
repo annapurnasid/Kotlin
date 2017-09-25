7163b494fac2c6dfc1abd453410b270996efb66b
home <smartherd@gmail.com>
2017-09-25 10:16:55 +0530

    Lambdas and High Level Functions

--- src/FirstKotlinDemo.kt
+++ src/FirstKotlinDemo.kt
@@ -1,27 +1,42 @@
 /*
-* Loop Control Statements
+* Lambdas and High Level Functions
 *
-* Break and Continue
+* Lambdas - Functions without name
+* High Level Functions
+* - accepts function as arguments
+* - Returns function
+* - Both of them
+*
+* - Important for Android Application Development
 * */
 fun main(args : Array<String>) {
 
-    // Break with Labelled FOR
-    primaryLoop@ for(i in 1..5) {
-        secondaryLoop@ for(j in 1..5) {
-            println("i = $i ; j = $j")
-            if (i == 3 && j == 3)
-                break@primaryLoop
-        }
+    var objDemo = Demo()
+    objDemo.getGreater(2, 4)
+
+    var testLambda : (Int) -> (Unit) = {num : Int -> println("Greater no is: $num") }
+
+    objDemo.getGreater(2, 4, testLambda)
+
+    objDemo.getGreater(4, 0, {num : Int -> println("Greater no is: $num") })
+
+}
+
+class Demo {
+    fun getGreater (num1 : Int, num2 : Int) : Unit {
+        var big = if (num1 > num2)
+            num1
+        else
+            num2
+        println("Greater is: $big")
     }
 
-    println ("========")
-
-    // Continue with Labelled FOR
-    primaryLoop@ for(i in 1..3) {
-        secondaryLoop@ for(j in 1..3) {
-            if (i == 2 && j == 2)
-                continue@primaryLoop
-            println("i = $i ; j = $j")
-        }
+    // High level function with Lambda as parameter
+    fun getGreater(num1 : Int, num2 : Int, myLambda : (Int) -> (Unit)) {
+        var big = if (num1 > num2)
+            num1
+        else
+            num2
+        myLambda(big) // println("Greater no is: $big")
     }
 }
